61a3e4bdb4e55a88e50a77ac8b3109d98fef0ab9
Matheus <matheus.augustoreal@gmail.com>
2022-04-13 05:47:17 -0300

    Create posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de abril de 2022</time>
+            <strong>Código Limpo: reflexão e prática</strong>
+            <p>Um conjunto de filosofias extremamente importantes e populares no ecossistema</p>
+          </a>
+          <a href="#">
+            <time>12 de abril de 2022</time>
+            <strong>Código Limpo: reflexão e prática</strong>
+            <p>Um conjunto de filosofias extremamente importantes e populares no ecossistema</p>
+          </a>
+          <a href="#">
+            <time>12 de abril de 2022</time>
+            <strong>Código Limpo: reflexão e prática</strong>
+            <p>Um conjunto de filosofias extremamente importantes e populares no ecossistema</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
