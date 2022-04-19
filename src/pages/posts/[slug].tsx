764981734b55bc73a7242503dc6062e955ec7b63
Matheus <matheus.augustoreal@gmail.com>
2022-04-19 06:54:30 -0300

    Post preview redirecting

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -45,7 +45,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     console.log(session)
     return {
       redirect: {
-        destination: '/',
+        destination: `posts/preview/${slug}`,
         permanent: false,
       }
     }
